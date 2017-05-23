c11f3cf73a7b52ac51daed5731351fe9081ab0a9
Eserophe Ovie-Okoro <eseropheoo@globalsparta.co>
2017-05-23 12:04:01 +0100

    shuffle array for each card type

---
+++ snap.js
@@ -0,0 +1,44 @@
+$(function() {
+
+// Create a variable for an array of 
+// random
+// on click
+// if statements to match
+// alert
+// 
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+var blackSpadesCard = [1, 2, 3, 4, 5];
+var blackClubsCard = [1, 2, 3, 4, 5];
+var redHeartsCard = [1, 2, 3, 4, 5];
+var redDiamondsCard = [1, 2, 3, 4, 5];
+
+
+blackSpadesCard = shuffle(blackSpadesCard);
+console.log('blackSpadesCard', blackSpadesCard);
+blackClubsCard = shuffle(blackClubsCard);
+console.log('blackClubsCard', blackClubsCard);
+redDiamondsCard = shuffle(redDiamondsCard);
+console.log('redDiamondsCard', redDiamondsCard);
+redHeartsCard = shuffle(redHeartsCard);
+console.log('redDiamondsCard', redDiamondsCard);
+
+});
