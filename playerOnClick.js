bf3b7447ed433766213c1abe634cf4045a3fb7d5
Eserophe Ovie-Okoro <eseropheoo@globalsparta.co>
2017-05-24 17:26:50 +0100

    player and computer can both snap on demand

--- playerOnClick.js
+++ playerOnClick.js
@@ -1,32 +1,54 @@
+var computerSnapTimeout;
+
+function checkSnap(person) {
+	var currCard = discard[discard.length-1];
+	var secLastCard = discard[discard.length-2];
+
+	if (currCard && secLastCard && currCard[0] === secLastCard[0]){
+		alert(person +  " snap!")
+		// 2. output snap to screen
+		// 3. move all cards from discard array to player or cpu
+	}
+
+}
+
+
 
 function changeCard() {
+	checkSnap("cpu");
+	
 	console.log("playing a cpu card...")
 	var newcard = cpu.pop();
+	// 4. check to see if cpu has any chards left, if not, they have lost the game
 	discard.push(newcard);
 	console.log("cpu discard", newcard);
 	$("#card01").html(newcard);
 
+
+	computerSnapTimeout = setTimeout(function () {
+		checkSnap("computer")
+	}, 2500);
+	// 1. player ones turn
 }
+
+	
+
 $(".card").click(function() {
 	console.log("Playing a player card...")
 	var newcard = shuffle.pop();
+	// check to see if player has any chards left, if not, they have lost the game
+
 	discard.push(newcard);
 	console.log("player discard", newcard);
 	$("#card01").html(newcard);
 	setTimeout(changeCard, 3000);
+	// 1. computer's turn
 })
  
  
-
-
-
 $("#snap").click(function(){
-	var currCard = discard[discard.length-1];
-	var secLastCard = discard[discard.length-2];
-	
- 	if (currCard[0] === secLastCard[0]){
-		alert("snap");
-	}
+	clearTimeout(computerSnapTimeout);
+	checkSnap("player");	
 })	
 
  
