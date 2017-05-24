8bee78f82ba20dead1b460c8a705bde74c02220a
Eserophe Ovie-Okoro <eseropheoo@globalsparta.co>
2017-05-24 13:47:40 +0100

    corrected player and cpu deck draw

--- playerOnClick.js
+++ playerOnClick.js
@@ -1,14 +1,16 @@
-$(function() {
-	function changeCard() {
-		console.log("clickec on card")
-		var newcard = shuffle.pop();
-		$("#card01").html(newcard);
-	}
-	$(".card").click(function() {
-		console.log("clickec on card")
-		var newcard = shuffle.pop();
-		$("#card01").html(newcard);
-		setTimeout(changeCard, 5000);
-	})
+
+function changeCard() {
+	console.log("playing a cpu card...")
+	var newcard = cpu.pop();
+	$("#card01").html(newcard);
+}
+$(".card").click(function() {
+	console.log("Playing a player card...")
+	var newcard = shuffle.pop();
+	$("#card01").html(newcard);
+	setTimeout(changeCard, 5000);
 })
+
+ 
+
 		// setTimeout(changeCard, 5000);
--- snap.js
+++ snap.js
@@ -1,4 +1,4 @@
-$(function() {
+
 
 
 function shufflePack(array) {
@@ -25,15 +25,15 @@ var cards = ["1 &clubs;", "2 &clubs;", "3 &clubs;", "4 &clubs;", "5 &clubs;", "6
 "1 &spades;", "2 &spades;", "3 &spades;","4 &spades;", "5 &spades;", "6 &spades;", "7 &spades;", "8 &spades;", "9 &spades;", "10 &spades;", "J &spades;", "Q &spades;", "K &spades;",
  "1 &hearts;", "2 &hearts;", "3 &hearts;", "4 &hearts;", "5 &hearts;", "6 &hearts;", "7 &hearts;", "8 &hearts;", "9 &hearts;", "10 &hearts;","J &hearts;", "Q &hearts;", "K &hearts;",
  "1 &#9830;", "2 &#9830;", "3 &#9830;", "4 &#9830;", "5 &#9830;", "6 &#9830;", "7 &#9830;", "8 &#9830;", "9 &#9830;", "10 &#9830;", "J  &#9830;", "Q &#9830;", "K &#9830;"]
-shuffle = [];
+var shuffle = [];
 var x;
 
 // var player = cards.slice(0, 26);
 // var cpu = cards.slice(26, 52);
 
-   function rdm(n){
- 	 x = Math.floor(Math.random() * n);
- }
+function rdm(n){
+	x = Math.floor(Math.random() * n);
+}
 
 for( i = 0; i < 52; i++) {
 	rdm(cards.length);
@@ -71,16 +71,16 @@ function playerMove(playerCards) {
       // console.log("playerCards", playerCards);
 
     
-  function cpuMove (cpuCards) {
-    for (i =26; i < 51; i++) {
-      rdm(cpuCards);
-      shuffle.push(cpuCards[x]);
-      cpuCards.splice(x, 1);
-    }
-    console.log("cpuMove", cpuCards); 
-
+function cpuMove (cpuCards) {
+  for (i =26; i < 51; i++) {
+    rdm(cpuCards);
+    shuffle.push(cpuCards[x]);
+    cpuCards.splice(x, 1);
   }
+  console.log("cpuMove", cpuCards); 
 
- })
+}
+
+ 
 
 
