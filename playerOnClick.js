49140eb266ac7a8cbfce9c558a4b5852364c4eb1
Eserophe Ovie-Okoro <eseropheoo@globalsparta.co>
2017-05-24 12:39:19 +0100

    cpu plays after player click but takes from player deck

--- playerOnClick.js
+++ playerOnClick.js
@@ -1,7 +1,14 @@
 $(function() {
-	$(".card").click(function () {
+	function changeCard() {
 		console.log("clickec on card")
 		var newcard = shuffle.pop();
 		$("#card01").html(newcard);
+	}
+	$(".card").click(function() {
+		console.log("clickec on card")
+		var newcard = shuffle.pop();
+		$("#card01").html(newcard);
+		setTimeout(changeCard, 5000);
 	})
 })
+		// setTimeout(changeCard, 5000);
